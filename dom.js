86bd1a4501e33286a91015fd0089b17faa0fe98e
sreekanthmandala1 <sreekanthmandalaa@gmail.com>
2023-10-20 23:36:09 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,7 @@
+var header = document.querySelector('h1');
+
+header.addEventListener("click",function(){
+    header.innerHTML = 'changedddd'
+    header.style.backgroundColor = "#000";
+    header.style.color = "#fff";
+})
